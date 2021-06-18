37e4ea02db4a70ebe19b456122431218c2cacfaa
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2021-06-18 16:37:49 +0200

    Extract method checking if the name is a mononym

--- name-normalizer/src/main/kotlin/namenormalizer/AuthorNameNormalizer.kt
+++ name-normalizer/src/main/kotlin/namenormalizer/AuthorNameNormalizer.kt
@@ -3,9 +3,12 @@ package namenormalizer
 class AuthorNameNormalizer {
     fun normalize(name: String): String {
         val parts = name.split(" ")
-        if (parts.count() == 1) {
+        if (isMononym(parts)) {
             return name
         }
         return parts[1] + ", " + parts[0]
     }
+
+    private fun isMononym(parts: List<String>): Boolean =
+        parts.count() == 1
 }
