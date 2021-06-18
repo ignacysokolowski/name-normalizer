712f530db18c72a8bc0db9919f09b8e894bfcdf2
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2021-06-18 16:46:33 +0200

    Move splitting to the NameParts

--- name-normalizer/src/main/kotlin/namenormalizer/AuthorNameNormalizer.kt
+++ name-normalizer/src/main/kotlin/namenormalizer/AuthorNameNormalizer.kt
@@ -2,18 +2,20 @@ package namenormalizer
 
 class AuthorNameNormalizer {
     fun normalize(name: String): String {
-        val nameParts = namePartsOf(name)
+        val nameParts = NameParts.of(name)
         if (nameParts.isMononym()) {
             return name
         }
         return nameParts.lastName() + ", " + nameParts.firstName()
     }
-
-    private fun namePartsOf(name: String): NameParts =
-        NameParts(name.trim().split(" "))
 }
 
 class NameParts(private val parts: List<String>) {
+    companion object {
+        fun of(name: String): NameParts =
+            NameParts(name.trim().split(" "))
+    }
+
     fun isMononym(): Boolean =
         parts.count() == 1
 
