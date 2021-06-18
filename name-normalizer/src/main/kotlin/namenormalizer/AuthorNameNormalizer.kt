2b95fd474c701ff879030587b578ff9a2d17f7a6
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2021-06-18 16:42:01 +0200

    Make parts private

--- name-normalizer/src/main/kotlin/namenormalizer/AuthorNameNormalizer.kt
+++ name-normalizer/src/main/kotlin/namenormalizer/AuthorNameNormalizer.kt
@@ -10,7 +10,7 @@ class AuthorNameNormalizer {
     }
 }
 
-class NameParts(val parts: List<String>) {
+class NameParts(private val parts: List<String>) {
     fun isMononym(): Boolean =
         parts.count() == 1
 
